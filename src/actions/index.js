243c31142bbb08745fd4c50b7a580d6641c58401
Maciej Kurek <maciejk77@gmail.com>
2018-03-25 20:17:08 +0100

    App working, no Sparklines added yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'd829725a0607d581d25b291da3f68d1c';
+const API_KEY = '71230211a016e57d54afb29cf481b19f';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -8,8 +8,6 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
-
-	console.log('Request:', request);
 
 	return {
 		type: FETCH_WEATHER,
